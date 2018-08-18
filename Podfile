81ca55a01b457560827044d2d5612bae59375836
hassaneldesouky <hassaneldesouky@icloud.com>
2018-08-18 10:06:18 +0200

    add screenshots

---
+++ Podfile
@@ -0,0 +1,15 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
+use_frameworks!
+
+target 'mashroo' do
+pod 'Alamofire', '~> 4.5'
+pod 'SwiftyJSON'
+pod 'Toaster'
+pod 'MaterialComponents/AppBar'
+pod 'MaterialComponents/Buttons'
+pod 'MaterialComponents/Collections'
+
+
+
+end
---
+++ Pods/Target Support Files/MaterialComponents/MaterialComponents-umbrella.h
@@ -0,0 +1,79 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "MaterialAppBar.h"
+#import "MDCAppBar.h"
+#import "MDCAppBarContainerViewController.h"
+#import "MaterialButtonBar.h"
+#import "MDCButtonBar.h"
+#import "MDCButtonBarButton.h"
+#import "MaterialButtons.h"
+#import "MDCButton.h"
+#import "MDCFlatButton.h"
+#import "MDCFloatingButton+Animation.h"
+#import "MDCFloatingButton.h"
+#import "MDCRaisedButton.h"
+#import "MaterialCollectionCells.h"
+#import "MDCCollectionViewCell.h"
+#import "MDCCollectionViewTextCell.h"
+#import "MaterialCollectionLayoutAttributes.h"
+#import "MDCCollectionViewLayoutAttributes.h"
+#import "MaterialCollections.h"
+#import "MDCCollectionViewController.h"
+#import "MDCCollectionViewEditing.h"
+#import "MDCCollectionViewEditingDelegate.h"
+#import "MDCCollectionViewFlowLayout.h"
+#import "MDCCollectionViewStyling.h"
+#import "MDCCollectionViewStylingDelegate.h"
+#import "MaterialFlexibleHeader.h"
+#import "MDCFlexibleHeaderContainerViewController.h"
+#import "MDCFlexibleHeaderView.h"
+#import "MDCFlexibleHeaderViewController.h"
+#import "MaterialHeaderStackView.h"
+#import "MDCHeaderStackView.h"
+#import "MaterialInk.h"
+#import "MDCInkGestureRecognizer.h"
+#import "MDCInkTouchController.h"
+#import "MDCInkView.h"
+#import "MaterialNavigationBar.h"
+#import "MDCNavigationBar.h"
+#import "MaterialPalettes.h"
+#import "MDCPalettes.h"
+#import "MaterialShadowElevations.h"
+#import "MDCShadowElevations.h"
+#import "MaterialShadowLayer.h"
+#import "MDCShadowLayer.h"
+#import "MaterialTypography.h"
+#import "MDCFontTextStyle.h"
+#import "MDCTypography.h"
+#import "UIFont+MaterialTypography.h"
+#import "UIFontDescriptor+MaterialTypography.h"
+#import "MaterialApplication.h"
+#import "UIApplication+AppExtensions.h"
+#import "MaterialIcons.h"
+#import "MDCIcons+BundleLoader.h"
+#import "MDCIcons.h"
+#import "MaterialIcons+ic_arrow_back.h"
+#import "MaterialIcons+ic_check.h"
+#import "MaterialIcons+ic_check_circle.h"
+#import "MaterialIcons+ic_chevron_right.h"
+#import "MaterialIcons+ic_info.h"
+#import "MaterialIcons+ic_radio_button_unchecked.h"
+#import "MaterialIcons+ic_reorder.h"
+#import "MaterialMath.h"
+#import "MDCMath.h"
+#import "MaterialUIMetrics.h"
+#import "MDCLayoutMetrics.h"
+
+FOUNDATION_EXPORT double MaterialComponentsVersionNumber;
+FOUNDATION_EXPORT const unsigned char MaterialComponentsVersionString[];
+
